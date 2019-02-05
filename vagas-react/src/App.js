8a5b376fac78845c42e01d176f866095e020605f
4tech <4tech@suportes-Mac-mini-7.local>
2019-02-05 15:57:02 -0200

    JobsList and Form

--- vagas-react/src/App.js
+++ vagas-react/src/App.js
@@ -2,17 +2,19 @@ import React, {Component} from 'react';
 import './App.css';
 
 import Header from './components/navigation/Header/Header'
+import Form from './components/navigation/Form/Form'
 import Jobs from './components/navigation/Job/JobsList'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Header/>
-        <div class="row"><Jobs/></div>
-      </div>
-    );
-  }
+  render= () =>(
+    <div className="App">
+      <Header/>
+      <Form/>
+      <div className="row">
+        <Jobs/>
+      </div>        
+    </div>
+  )
 }
 
 export default App;
